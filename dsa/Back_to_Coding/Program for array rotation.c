c3390e4bd2b11bf6efa649daf6dbdb29fb8ce651
vimalpundir10 <40239586+vimalpundir10@users.noreply.github.com>
2020-10-08 10:49:20 +0530

    Add files via upload

---
+++ dsa/Back_to_Coding/Program for array rotation.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main(int)
+{
+	printf("Hello, world");
+	return 0;
+}
---
+++ dsa/Back_to_Coding/Rearrange positive and negative numbers in O(n) time and O(1) extra space.py
@@ -0,0 +1,22 @@
+def reArrange(arr, n):
+	i = -1
+	for j in range(n):
+		if arr[j] < 0:
+			i += 1
+			arr[i], arr[j] = arr[j], arr[i]
+	pos, neg = i + 1, 0
+	while pos < n and neg < pos and arr[neg] < 0:
+		arr[neg], arr[pos] = arr[pos], arr[neg]
+		pos += 1
+		neg += 2
+
+
+def printArray(arr, n):
+	for i in range(n):
+		print(arr[i], end=" ")
+
+
+arr = [-1, 2, -3, 4, 5, 6, -7, 8, 9]
+n = len(arr)
+reArrange(arr, n)
+printArray(arr, n)
